154001afcc65a04070fac5b4453f0527cfb2aec3
Antonio Pereira <amcp.62@gmail.com>
2022-02-08 17:28:04 +0000

    Initial Commit

---
+++ SourceControlXcode13/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SourceControlXcode13
+//
+//  Created by Antonio Pereira on 08/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SourceControlXcode13/SourceControlXcode13App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlXcode13App.swift
+//  SourceControlXcode13
+//
+//  Created by Antonio Pereira on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlXcode13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
